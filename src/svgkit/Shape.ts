73b3dd6ea9f8ca2c9e90dd6fd95194fee1f52934
Tom Schindl <tom.schindl@bestsolution.at>
2018-04-18 22:49:03 +0200

    added circle

--- src/svgkit/Shape.ts
+++ src/svgkit/Shape.ts
@@ -53,6 +53,16 @@ export class SkRect extends SkNode implements IShape {
         return c;
     }
 
+    // styling
+    get class() : string | stringList { return this.prop("class") }
+    set class( clazz : string | stringList ) { this.prop("class", clazz) }
+
+    get style() : string { return this.prop("style") }
+    set style( style : string ) { this.prop("style", style) }
+
+    get externalResourcesRequired() : boolean { return Boolean(this.prop("externalResourcesRequired")) }
+    set externalResourcesRequired( externalResourcesRequired : boolean ) { this.prop("externalResourcesRequired", String(externalResourcesRequired)) }
+
     get x() : coordinate { return this.prop("x") }
     set x( x : coordinate ) { this.prop("x", stringify(x) ) }
 
@@ -96,5 +106,37 @@ export class SkRect extends SkNode implements IShape {
 }
 
 export class SkCircle extends SkNode {
+    static create( init? : ( obj : SkCircle ) => void ) : SkCircle {
+        return SkCircle.adapt(document.createElementNS(SVG_NAMESPACE,"circle"), init);
+    }
 
+    static adapt( element : Element, init? : ( obj : SkCircle ) => void) : SkCircle {
+        var c = element["sk"];
+        c = c ? c : new SkCircle(element)
+
+        if( init ) {
+            init.call( null, c)
+        }
+
+        return c;
+    }
+
+    // styling
+    get class() : string | stringList { return this.prop("class") }
+    set class( clazz : string | stringList ) { this.prop("class", clazz) }
+
+    get style() : string { return this.prop("style") }
+    set style( style : string ) { this.prop("style", style) }
+
+    get externalResourcesRequired() : boolean { return Boolean(this.prop("externalResourcesRequired")) }
+    set externalResourcesRequired( externalResourcesRequired : boolean ) { this.prop("externalResourcesRequired", String(externalResourcesRequired)) }
+
+    get cx() : string | coordinate { return this.prop("cx") }
+    set cx( cx : string | coordinate ) { this.prop("cx",stringify(cx)) }
+
+    get cy() : string | coordinate { return this.prop("cy") }
+    set cy( cy : string | coordinate ) { this.prop("cy",stringify(cy)) }
+
+    get r() : string | length { return this.prop("r") }
+    set r( r : string | length ) { this.prop("r",stringify(r)) }
 }
